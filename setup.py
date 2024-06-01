1a3bba5b5299cfc7c0cf04d4da6c1dbc1281ad7c
Valdecy Pereira <valdecypereira@yahoo.com.br>
2024-05-31 22:20:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='ga_scheduler',
-    version='2.0.1',
+    version='2.0.2',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
